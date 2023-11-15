9917dd07372734517a374850d1a4bf66af5249a6
marufbellah <mdmaruf5511507@gmail.com>
2023-11-15 17:05:50 +0600

    every code uncommented for pushing github

--- 4.class/index.js
+++ 4.class/index.js
@@ -1,71 +1,71 @@
-// const names = [
-//   "HM Nayeem",
-//   "Aditya Chakraborty",
-//   "Abu Rayhan",
-//   "Shaker Hossain",
-//   "Akib Ahmad",
-//   "Alvi Chowdhury",
-// ];
+const names = [
+  "HM Nayeem",
+  "Aditya Chakraborty",
+  "Abu Rayhan",
+  "Shaker Hossain",
+  "Akib Ahmad",
+  "Alvi Chowdhury",
+];
 
-// let index = 4;
-// let person = names[index++];
+let index = 4;
+let person = names[index++];
 
-// setInterval(() => {
-//   //   person = names[index++];
-//   console.log(person);
+setInterval(() => {
+  //   person = names[index++];
+  console.log(person);
 
-//   if (index === names.length) {
-//     index = 4;
-//   }
-// }, 1000);
+  if (index === names.length) {
+    index = 4;
+  }
+}, 1000);
 
-// const persons = ["Rayhan", "Alvi", "Anik", "Arjun", "Ayman", "Maruf", "Bellah"];
+const persons = ["Rayhan", "Alvi", "Anik", "Arjun", "Ayman", "Maruf", "Bellah"];
 
-// // console.log(persons[1]);
-// // console.log(persons[2]);
-// // console.log(persons[3]);
-// // console.log(persons[4]);
-// // console.log(persons[5]);
-// // console.log(persons[6]);
+console.log(persons[1]);
+console.log(persons[2]);
+console.log(persons[3]);
+console.log(persons[4]);
+console.log(persons[5]);
+console.log(persons[6]);
 
-// for (let i = 0; i < 5; i++) {
-//   //   console.log(persons[i], persons[i].toLocaleLowerCase());
-// }
+for (let i = 0; i < 5; i++) {
+  //   console.log(persons[i], persons[i].toLocaleLowerCase());
+}
 
-// const nums = [1, 2, 3, 4, 5, 6];
-// const bools = [true, true, false, false];
-// const nulls = [null, null, null];
-// const undefineds = [undefined, undefined, undefined];
-// const arrayOfArray = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ];
-// const mixed = [true, null, "Str", 5, [12, 2, 4]];
-// 1;
+const nums = [1, 2, 3, 4, 5, 6];
+const bools = [true, true, false, false];
+const nulls = [null, null, null];
+const undefineds = [undefined, undefined, undefined];
+const arrayOfArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+const mixed = [true, null, "Str", 5, [12, 2, 4]];
+1;
 
-// console.log(mixed);
+console.log(mixed);
 
-// const students = ["Abu", "Rayhan", "rayhan@example.com", 25, true];
-// sendEmail(students[0]);
+const students = ["Abu", "Rayhan", "rayhan@example.com", 25, true];
+sendEmail(students[0]);
 
-// function sendEmail(email) {
-//   //   console.log("sending Email" + " " + email);
-// }
+function sendEmail(email) {
+  console.log("sending Email" + " " + email);
+}
 
-// const student = {
-//   firstName: "Maruf",
-//   lastName: "Bellah",
-//   email: "mdmaruf5511507@gamil.com",
-//   age: 25,
-//   attend: true,
-// };
+const student = {
+  firstName: "Maruf",
+  lastName: "Bellah",
+  email: "mdmaruf5511507@gamil.com",
+  age: 25,
+  attend: true,
+};
 
-// sendEmails(student["firstName"], student["lastName"]);
+sendEmails(student["firstName"], student["lastName"]);
 
-// function sendEmails(email, name) {
-//   console.log("sending Email" + " " + email, name);
-// }
+function sendEmails(email, name) {
+  console.log("sending Email" + " " + email, name);
+}
 
 const student1 = {
   firstName: "Abu",
@@ -98,13 +98,13 @@ for (let i = 0; i < allStudents.length; i++) {
 }
 
 function showStudent(email) {
-  //   console.log("sending email to all student " + " " + email);
+  console.log("sending email to all student " + " " + email);
 }
 
-// allStudents.forEach((item) => console.log("email", " " + item.email));
+allStudents.forEach((item) => console.log("email", " " + item.email));
 
 allStudents.forEach((item) => {
-  //   console.log("Eamil" + " " + item.email);
+  console.log("Eamil" + " " + item.email);
 });
 
 function nameOfFunction(name) {
