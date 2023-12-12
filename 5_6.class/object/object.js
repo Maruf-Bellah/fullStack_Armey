aa9f12a5ab4f2b91c9e218879b4c52957020fef6
marufbellah <mdmaruf5511507@gmail.com>
2023-12-12 12:31:38 +0600

    object curd operaton is done

--- 5_6.class/object/object.js
+++ 5_6.class/object/object.js
@@ -96,13 +96,13 @@
 
 // Object as a Data Structure  ==================================
 
-// function uuidv4() {
-//   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-//     const r = (Math.random() * 16) | 0;
-//     const v = c == "x" ? r : (r & 0x3) | 0x8;
-//     return v.toString(16);
-//   });
-// }
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 // Array==========================================
 
@@ -216,3 +216,54 @@
 
 
 // Object Over Array=============================
+
+const students = {
+  "67de71e5-0eac-474f-ab51-850ba9b31ed5": {
+    id: "67de71e5-0eac-474f-ab51-850ba9b31ed5",
+    name: "Md Al-Amin",
+    email: "alamin@test.com",
+  },
+ "ebdf6b78-c32b-4b1d-8574-e8c655b05c1e": {
+    id: "ebdf6b78-c32b-4b1d-8574-e8c655b05c1e",
+    name: "Akib Ahmed",
+    email: "akib@test.com",
+  },
+  "ee729e84-a84e-4adf-b32c-4647a7114d5b": {
+    id: "ee729e84-a84e-4adf-b32c-4647a7114d5b",
+    name: "Elias Emon",
+    email: "elias@test.com",
+  },
+}
+
+// const std = {
+//   id: uuidv4(),
+//   name: "Feroz Khan",
+//   email: "feroz@test.com",
+// };
+// students[std.id] = std;
+
+
+// console.log(students);
+
+
+
+// update ================
+
+console.log(students);
+
+const idToBeUpdated = "ee729e84-a84e-4adf-b32c-4647a7114d5b";
+const updatedData = {
+  name: "HM Azizul",
+  email: "azizul@test.com",
+};
+
+students[idToBeUpdated] = {
+  ...students[idToBeUpdated],
+  ...updatedData,
+};
+
+
+// delete =================
+// delete students[idToBeUpdated]
+
+// console.log(students);
