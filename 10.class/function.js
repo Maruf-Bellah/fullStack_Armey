979f3d7ee8bdd64d16c6a30ae0f34f714ffb5a01
marufbellah <mdmaruf5511507@gmail.com>
2023-12-23 15:56:21 +0600

    promise is done

--- 10.class/function.js
+++ 10.class/function.js
@@ -30,20 +30,58 @@
 
 // console.log(8);
 
-
 // callback function ===============
 
 function main() {
-    setTimeout(() => {
-        console.log('load last');
-    }, 2000)
-    setTimeout(() => {
-        console.log('load first');
-    }, 1000);
-    test()
-};
+  setTimeout(() => {
+    console.log("load last");
+  }, 2000);
+  setTimeout(() => {
+    console.log("load first");
+  }, 1000);
+  test();
+}
 
 function test() {
-    console.log('test');
+  console.log("test");
 }
-main()
+// main()
+
+// promise ========================
+
+const isResolved = false;
+
+const promise = new Promise((resolve, reject) => {
+  if (isResolved) {
+    resolve("completed the true");
+  } else {
+    reject("something is went wrong");
+  }
+});
+
+// console.log(promise);
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+
+// function wait(ms) {
+//     const promise = new Promise((resolve) => {
+//         setTimeout(resolve, ms)
+//     });
+//     return promise;
+// };
+
+const wait = (ms) => new Promise((resolve)=> setTimeout(resolve, ms))
+wait(1000).then(() => {
+    console.log('Done in 1000ms');
+})
+
+wait(2000).then(() => {
+  console.log("Done in 2000ms");
+});
