44d20204339b39fcdf85acc1d7a274599abc87f0
marufbellah <mdmaruf5511507@gmail.com>
2023-12-23 15:28:37 +0600

    syncronus is done

---
+++ 10.class/function.js
@@ -0,0 +1,31 @@
+// Synchronous ============
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+
+// Asynchronous ============
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 0);
+// setTimeout(() => {
+//   console.log(3);
+// }, 0);
+// setTimeout(() => {
+//   console.log(4);
+// }, 0);
+// setTimeout(() => {
+//   console.log(5);
+// }, 0);
+// setTimeout(() => {
+//   console.log(6);
+// }, 0);
+// setTimeout(() => {
+//   console.log(7);
+// }, 0);
+
+// console.log(8);
