30e514b7518b8763be2dc38c4f35ea2bb7e1e31b
marufbellah <mdmaruf5511507@gmail.com>
2023-12-24 17:16:15 +0600

    function is done

--- 10.class/function.js
+++ 10.class/function.js
@@ -51,24 +51,23 @@ function test() {
 
 const isResolved = false;
 
-const promise = new Promise((resolve, reject) => {
-  if (isResolved) {
-    resolve("completed the true");
-  } else {
-    reject("something is went wrong");
-  }
-});
+// const promise = new Promise((resolve, reject) => {
+//   if (isResolved) {
+//     resolve("completed the true");
+//   } else {
+//     reject("something is went wrong");
+//   }
+// });
 
 // console.log(promise);
 
-promise
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-
+// promise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
 
 // function wait(ms) {
 //     const promise = new Promise((resolve) => {
@@ -77,11 +76,76 @@ promise
 //     return promise;
 // };
 
-const wait = (ms) => new Promise((resolve)=> setTimeout(resolve, ms))
-wait(1000).then(() => {
-    console.log('Done in 1000ms');
-})
+// const wait = (ms) => new Promise((resolve)=> setTimeout(resolve, ms))
+// wait(1000).then(() => {
+//     console.log('Done in 1000ms');
+// })
 
-wait(2000).then(() => {
-  console.log("Done in 2000ms");
-});
+// wait(2000).then(() => {
+//   console.log("Done in 2000ms");
+// });
+
+// const get = (url) => Promise.resolve();
+// get(`/users?username=Maruf`)
+//     .then((user) => {
+//         return get(`/posts?user_id=${user.id}`);
+//     })
+//     .then((posts) => {
+//         const latestPost = posts[0];
+//         return get(`/comments?post_id=${latestPost.id}`);
+//     })
+//     .then((comments) => {
+//         const latestComment = comments[0];
+//         return get(`/users?username=${latestComment.username}`);
+
+//     })
+//     .then((user) => {
+//         console.log(user);
+//     })
+//     .catch(() => {
+//         console.log('Error');
+//     });
+
+// eta kicu buji nai =======================================
+
+// const get = (url) => Promise.resolve();
+
+// async function getUserName(username) {
+//   try {
+//     const mainUser = await get(`/users?username=${username}`);
+//     const posts = await get(`/posts?user_id=${mainUser.id}`);
+//     const comments = await get(`comments?post_id${posts[0].id}`);
+//     const user = await get(`/users?username=${comments[0].username}`);
+//     console.log(user);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// getUserName(url);
+
+const axios = require("axios").default;
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+const COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+
+async function getComments(username) {
+  try {
+    const { data: user } = await axios.get(`${USERS_URL}?username=${username}`);
+    const { data: posts } = await axios.get(
+      `${POSTS_URL}?userId=${user[0].id}`
+    );
+    const { data: comments } = await axios.get(
+      `${COMMENTS_URL}?postId=${posts[0].id}`
+    );
+
+    const { data: userWithComment } = await axios.get(
+      `${USERS_URL}?email=${comments[1].email}`
+    );
+    console.log(userWithComment);
+  } catch (error) {
+    console.log("Error Occurred", error.toJSON());
+  }
+}
+
+getComments("Bret");
