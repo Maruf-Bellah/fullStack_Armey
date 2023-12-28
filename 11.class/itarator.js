c441e34f993d041e3d77d9e41425ebef28a94af7
marufbellah <mdmaruf5511507@gmail.com>
2023-12-28 11:00:02 +0600

    symbol iterator done

--- 11.class/itarator.js
+++ 11.class/itarator.js
@@ -1,6 +1,5 @@
 // itarator ek dhoroner ekta object,  jeta bortomaner kotha jane and  vovisshote e kichu ache kina seta o jane
 
-
 // We use iterator because we cannot pause a loop. When a loop starts it will not stop until the execution of last element. Let's look the below example:
 
 const arr = [1, 2, 3, 4];
@@ -18,3 +17,15 @@ function next() {
 // console.log(next());
 
 // In iterator if there is no value it returns simply undefined.
+
+const channel = "Maruf";
+const chanelIterator = channel[Symbol.iterator]();
+
+// console.log(chanelIterator.next());
+// console.log(chanelIterator.next());
+// console.log(chanelIterator.next());
+// console.log(chanelIterator.next());
+// console.log(chanelIterator.next());
+// console.log(chanelIterator.next());
+
+
