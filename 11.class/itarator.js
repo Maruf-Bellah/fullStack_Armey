4d4b2501880302437c7b18290a9204dd4746ce77
marufbellah <mdmaruf5511507@gmail.com>
2023-12-28 10:50:37 +0600

    itarator is done

---
+++ 11.class/itarator.js
@@ -0,0 +1,20 @@
+// itarator ek dhoroner ekta object,  jeta bortomaner kotha jane and  vovisshote e kichu ache kina seta o jane
+
+
+// We use iterator because we cannot pause a loop. When a loop starts it will not stop until the execution of last element. Let's look the below example:
+
+const arr = [1, 2, 3, 4];
+
+let index = 0;
+
+function next() {
+  return arr[index++];
+}
+
+// console.log(next());
+// console.log(next());
+// console.log(next());
+// console.log(next());
+// console.log(next());
+
+// In iterator if there is no value it returns simply undefined.
