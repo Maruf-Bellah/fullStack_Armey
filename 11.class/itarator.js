0f111884dbc47f5cff098afdd9f5638b525a1c39
marufbellah <mdmaruf5511507@gmail.com>
2023-12-28 11:08:42 +0600

    symbol with forloop

--- 11.class/itarator.js
+++ 11.class/itarator.js
@@ -28,4 +28,28 @@ const chanelIterator = channel[Symbol.iterator]();
 // console.log(chanelIterator.next());
 // console.log(chanelIterator.next());
 
+const range = {
+  start: 0,
+  stop: 100,
+  step: 5,
+};
 
+range[Symbol.iterator] = function () {
+    let current = this.start;
+    const stop = this.stop;
+    const step = this.step;
+    return {
+        next() {
+            const o = {
+                value: current,
+                done: current > stop,
+            };
+            current += step;
+            return o;
+        },
+    };
+};
+
+for (let v of range) {
+    console.log(v);
+}
